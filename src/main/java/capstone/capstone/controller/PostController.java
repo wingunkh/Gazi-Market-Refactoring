e10fbbb7318520d340fd662a93764c133839c81a
wingunkh <99gusrms@naver.com>
2023-02-21 12:17:13 +0900

    Controller 패키지 리팩터링

--- src/main/java/capstone/capstone/controller/PostController.java
+++ src/main/java/capstone/capstone/controller/PostController.java
@@ -8,37 +8,29 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")     // CORS 문제를 해결하기 위해 추가
-@RestController                                     // @Controller에 @ResponseBody가 결합, 문자열과 JSON 등을 전송 가능
-@RequestMapping("/api")                             // /api URL에서 PostController 실행됨
+@CrossOrigin(origins = "http://localhost:3000") // CORS 문제를 해결하기 위한 어노테이션
+@RestController // @Controller에 @ResponseBody가 결합한 어노테이션, 문자열과 JSON 등을 전송 가능
+@RequestMapping("/api") // 요청에 대해 어떤 Controller, 어떤 메소드가 처리할지를 맵핑하기 위한 어노테이션
 public class PostController {
-    @Autowired                                      // 필요한 의존 객체의 “타입"에 해당하는 빈을 찾아 주입, 생성자가 하나인 경우 입력하지 않아도 됨
+    @Autowired // 필요한 의존 객체의 “타입"에 해당하는 빈을 찾아 주입, 생성자가 하나인 경우 입력하지 않아도 된다.
     private PostService postService;
 
     //Service를 호출해서 글 목록의 데이터를 리턴하는 메소드
-    // get all post
-    @GetMapping("/post")                            // 어떠한 정보를 가져와서 조회하기 위해 사용되는 방식
+    @GetMapping("/post") // GET 방식: 정보를 단순히 조회하기 위해 사용하는 방식
     public List<Posts> getAllPosts() {
         return postService.getAllPost();
     }
 
     //Service를 호출해서 글을 저장하는 메소드
-    // create post
-    @PostMapping("/post")                           // 데이터를 서버로 제출하여 추가 또는 수정하기 위해서 데이터를 전송하는 방식
+    @PostMapping("/post") // POST 방식: 특정 데이터를 서버로 제출하여 해당 데이터를 추가, 수정 또는 삭제하기 위해 데이터를 전송하는 방식
     public Posts createPost(@RequestBody Posts post) {
         return postService.createPost(post);
     }
 
-    //get post
+    //Service를 호출해서 특정 게시글을 리턴하는 메소드
     @GetMapping("/post/{no}")
     public ResponseEntity<Posts> getPostByNo(
             @PathVariable Integer no) {
-
         return postService.getPost(no);
     }
-
-
-
-
-
 }

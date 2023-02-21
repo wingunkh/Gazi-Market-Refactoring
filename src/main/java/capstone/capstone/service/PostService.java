240e8878128406b9a42dc324ab333717e478ae6c
wingunkh <99gusrms@naver.com>
2023-02-21 12:35:22 +0900

    Service 패키지 리팩터링

--- src/main/java/capstone/capstone/service/PostService.java
+++ src/main/java/capstone/capstone/service/PostService.java
@@ -12,26 +12,17 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-    //Repository를 호출해서 글목록 데이터를 리턴하는 메소드.
-    // get boards data
     public List<Posts> getAllPost() {
         return postRepository.findAll();
     }
 
-    // create board
     public Posts createPost(Posts post) {
         return postRepository.save(post);
     }
 
-
-    // get post one by id
     public ResponseEntity<Posts> getPost(Integer no) {
         Posts post = postRepository.findById(no)
                 .orElseThrow(() -> new ResourceNotFoundException("Not exist Board Data by no : ["+no+"]"));
         return ResponseEntity.ok(post);
     }
-
-
-
-
 }

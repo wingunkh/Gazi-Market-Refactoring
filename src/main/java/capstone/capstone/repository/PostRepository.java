f4aeb00997f146c30b0e72b14ef8ea72f06f11ef
wingunkh <99gusrms@naver.com>
2023-02-21 12:21:32 +0900

    Repository 패키지 리팩터링

--- src/main/java/capstone/capstone/repository/PostRepository.java
+++ src/main/java/capstone/capstone/repository/PostRepository.java
@@ -3,7 +3,7 @@ package capstone.capstone.repository;
 import capstone.capstone.domain.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//간단한 CRUD기능은 JpaRepository를 상속하는 것으로 구현이 가능하다
+//간단한 CRUD 기능은 JpaRepository를 상속하는 것으로 구현이 가능하다.
 public interface PostRepository extends JpaRepository<Posts, Integer> {
 
 }

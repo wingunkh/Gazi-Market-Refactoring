4d50f5bc8ce9a53bec6ca02cdceb1aabcb6915f8
tjdgns99 <tjdgns1657@naver.com>
2023-04-12 18:43:25 +0900

    실시간 채팅 완료

---
+++ src/main/java/capstone/capstone/controller/SocketChatController.java
@@ -0,0 +1,34 @@
+package capstone.capstone.controller;
+
+import capstone.capstone.domain.Chatting;
+import capstone.capstone.service.ChattingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+
+@RestController
+public class SocketChatController {
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @Autowired
+    private ChattingService chattingService;
+
+
+    @MessageMapping("/chat/sendMessage")
+    public void sendMessage(@Payload Chatting chat) {   //@payload, 전송되는 데이터
+        System.out.println(chat.getCht_text());
+        chat.setCht_time(LocalDateTime.now());
+        chat.setCht_text(chat.getCht_text());
+        chattingService.createChatting(chat);
+        template.convertAndSend("/sub/chat/room/" + chat.getCht_room_num(), chat);
+    }
+
+}
+

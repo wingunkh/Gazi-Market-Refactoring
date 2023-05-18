fba9c41316f312d4cccef6aa96e7069fd961dbdf
wingunkh <99gusrms@naver.com>
2023-05-18 15:18:47 +0900

    이미지 판별 로직 수정(메타데이터 사용)

--- src/main/java/capstone/capstone/service/ImageSourceHandler.java
+++ src/main/java/capstone/capstone/service/ImageSourceHandler.java
@@ -1,15 +1,14 @@
 package capstone.capstone.service;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -23,35 +22,23 @@ public class ImageSourceHandler {
 
     public String detectImageSource(File imageFile) {
         try {
-            // OpenCV를 사용하여 이미지 로드
-            Mat image = Imgcodecs.imread(imageFile.getAbsolutePath());
+            FileInputStream inputStream = new FileInputStream(imageFile);
+            BufferedImage bufferedImage = ImageIO.read(inputStream);
+            String cameraMaker = (String) bufferedImage.getProperty("Make");
+            String cameraModel = (String) bufferedImage.getProperty("Model");
 
-            // 이미지가 성공적으로 로드되었는지 확인
-            if (image.empty()) {
-                return "???";
-            }
-
-            // 이미지를 그레이스케일로 변환
-            Mat grayImage = new Mat();
-            Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
-
-            // Canny 엣지 감지 적용
-            Mat edges = new Mat();
-            Imgproc.Canny(grayImage, edges, 50, 150);
-
-            // 감지된 엣지 개수 확인
-            int edgeCount = Core.countNonZero(edges);
-
-            // 엣지 개수에 기반하여 이미지 소스 판별
-            if (edgeCount > 0) {
+            // 메타데이터에 카메라 제조사와 모델 정보가 있으면 직접 촬영한 이미지로 간주
+            if(!StringUtils.isEmpty(cameraMaker) || !StringUtils.isEmpty(cameraModel)) {
                 return "CAPTURED";
-            } else {
+            } else { // 메타데이터에 카메라 제조사와 모델 정보가 없으면 다운로드 이미지로 간주
                 return "DOWNLOADED";
             }
-        } catch (Exception e) {
-            // 이미지 처리 중에 예외 발생 시 예외 처리
+
+        } catch (IOException e) {
+            // 이미지 파일을 읽는 중에 예외 발생
             e.printStackTrace();
-            return "???";
         }
+        return "???";
     }
 }
+

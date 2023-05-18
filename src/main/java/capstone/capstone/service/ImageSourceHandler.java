7be353d620f15931be3f3ee80f3e61dd479de4e9
wingunkh <99gusrms@naver.com>
2023-05-18 21:30:00 +0900

    이미지 판별 로직 수정(EXIF 데이터 사용)

--- src/main/java/capstone/capstone/service/ImageSourceHandler.java
+++ src/main/java/capstone/capstone/service/ImageSourceHandler.java
@@ -6,43 +6,28 @@ import com.drew.metadata.exif.ExifSubIFDDirectory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
 
 @Component
 public class ImageSourceHandler {
     public String detectImageSource(MultipartFile imageFile) {
         try {
-            ImageInputStream imageInputStream = ImageIO.createImageInputStream(imageFile.getInputStream());
-            ImageReader imageReader = ImageIO.getImageReaders(imageInputStream).next();
-            String formatName = imageReader.getFormatName();
-
-            // 이미지 포맷 이름이 null이거나 빈 문자열인 경우 다운로드 받은 이미지로 판별
-            if (StringUtils.isEmpty(formatName)) {
-                return "DOWNLOADED";
-            }
-
-            // EXIF 메타데이터를 읽어옴
+            // 이미지 파일의 메타데이터를 읽어온다.
             Metadata metadata = ImageMetadataReader.readMetadata(imageFile.getInputStream());
             System.out.println("metadata ===> " + metadata);
 
+            // EXIF 메타 데이터를 읽어오며 존재하지 않을 시 null 값이 저장된다.
             ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
             System.out.println("directory ===> " + directory);
 
-            String make = directory.getString(ExifSubIFDDirectory.TAG_MAKE);
-            System.out.println("make ===>" + make);
-
-            // "Make" 필드가 존재하지 않는 경우 다운로드 받은 이미지로 판별
-            if (StringUtils.isEmpty(make)) {
+            if (directory == null) {
                 return "DOWNLOADED";
+            } else {
+                return "CAPTURED";
             }
         } catch (Exception e) {
             // 이미지 파일을 읽을 수 없거나 예외가 발생한 경우 예외 처리
             e.printStackTrace();
+            return "???";
         }
-
-        // "Make" 필드가 존재하고 해당 값이 특정 값과 일치하는 경우 직접 촬영한 이미지로 판별
-        return "CAPTURED";
     }
 }

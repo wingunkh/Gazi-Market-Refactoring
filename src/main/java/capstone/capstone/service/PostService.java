87105a72629687e2c1f024c69ec1ceb623388fec
tjdgns99 <tjdgns1657@naver.com>
2023-05-24 17:40:45 +0900

    check

--- src/main/java/capstone/capstone/service/PostService.java
+++ src/main/java/capstone/capstone/service/PostService.java
@@ -42,6 +42,8 @@ public class PostService {
         postWithPicture.setPictureURL(pictureRepository.getPictureLocationByPostNo(post.getPost_num()));
         postWithPicture.setFairPrice(postRepository.findFairPrice(post.getModel_name(), post.getGrade()));
         postWithPicture.setLocation(postRepository.findLa(post.getUser_num()), postRepository.findLo(post.getUser_num()));
+        System.out.println(postWithPicture.getLocation().getLatitude());
+        System.out.println(postWithPicture.getLocation().getLatitude());
         postWithPicture.setProfile_image(userMemberRepository.showProfileImage(post.getUser_num()));
 
         return postWithPicture;

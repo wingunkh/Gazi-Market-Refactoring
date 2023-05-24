05c4fba3d8439e0027411993ec92548a15b3a0dd
tjdgns99 <tjdgns1657@naver.com>
2023-05-24 18:19:47 +0900

    profile image view

--- src/main/java/capstone/capstone/service/UserMemberService.java
+++ src/main/java/capstone/capstone/service/UserMemberService.java
@@ -16,7 +16,7 @@ public class UserMemberService {
     private FileHandler fileHandler;
 
     public String showProfileImage(int user_num) {
-        if(userMemberRepository.showProfileImage(user_num).isEmpty()){
+        if(userMemberRepository.showProfileImage(user_num) == null){
             return "https://capstone-eggplant-bucket.s3.ap-northeast-2.amazonaws.com/profile/default.png";
         }else {
             return userMemberRepository.showProfileImage(user_num);
@@ -27,5 +27,9 @@ public class UserMemberService {
         // Amazon S3에 전달받은 사진을 업로드하고 해당 사진의 Url이 담긴 Url 리스트를 반환받아 변수 list에 저장
         List<String> list = fileHandler.saveToS3(file, "profile/");
         userMemberRepository.updateProfileImage(user_num, list.get(0));
+    }
+
+    public String findName(int cht_member){
+        return  userMemberRepository.getNicknameByUserNum(cht_member);
     }
 }

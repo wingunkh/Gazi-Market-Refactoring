aa84dc1d768ed1051f320a24cac3071d59f28488
tjdgns99 <tjdgns1657@naver.com>
2023-05-24 16:37:14 +0900

    check

--- src/main/java/capstone/capstone/repository/UserMemberRepository.java
+++ src/main/java/capstone/capstone/repository/UserMemberRepository.java
@@ -9,7 +9,7 @@ public interface UserMemberRepository extends JpaRepository<User_Member, Integer
     @Query(value="select u.nickname from User_Member u where u.user_num = :user_num", nativeQuery = true)
     String getNicknameByUserNum(@Param("user_num") int user_num);
 
-    @Query(value="SELECT u.profile_image FROM User_Member u WHERE = :user_num", nativeQuery = true)
+    @Query(value="SELECT u.profile_image FROM User_Member u WHERE u.user_num = :user_num", nativeQuery = true)
     String showProfileImage(@Param("user_num") int user_num);
 
     @Query(value="UPDATE u.profile_image SET :profile_image FROM User_Member u WHERE u.user_num = :user_num", nativeQuery = true)

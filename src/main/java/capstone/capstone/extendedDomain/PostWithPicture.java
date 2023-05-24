26598a416bfd6dc603e5a418278dc045ba3523a6
tjdgns99 <tjdgns1657@naver.com>
2023-05-24 15:01:57 +0900

    에러 해결

--- src/main/java/capstone/capstone/extendedDomain/PostWithPicture.java
+++ src/main/java/capstone/capstone/extendedDomain/PostWithPicture.java
@@ -47,6 +47,7 @@ public class PostWithPicture extends Post {
     }
 
     public void setLocation(double latitude, double longitude) {
+        this.location = new Location();
         this.location.setLatitude(latitude);
         this.location.setLongitude(longitude);
     }

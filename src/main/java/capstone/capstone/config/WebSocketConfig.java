723bcf36d2a2b3f3934175be8f12c6ed551183e5
wingunkh <99gusrms@naver.com>
2023-12-16 17:25:29 +0900

    comment: config 패키지 주석 수정

--- src/main/java/capstone/capstone/config/WebSocketConfig.java
+++ src/main/java/capstone/capstone/config/WebSocketConfig.java
@@ -7,16 +7,25 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+// 스프링 컨테이너는 해당 어노테이션이 붙은 클래스를 설정(구성) 정보로 사용한다.
 @EnableWebSocketMessageBroker
+// 웹 소켓(Web Socket) 이란? 단방향 통신을 지원하는 HTTP와는 달리 양방향 통신을 지원하는 프로토콜이다.
+// HTTP 프로토콜을 통한 첫 번째 핸드셰이크를 주고받은 후 지속적으로 연결이 유지되는 것이 특징이다.
+// STOMP(Simple Text Oriented Messaging Protocol) 이란?
+// 클라이언트와 서버가 주고받을 메시지의 유형, 형식, 내용 등을 정의한 프로토콜이다. 웹 소켓 프로토콜과 함께 사용된다.
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    // 웹 소켓 엔드포인트 등록
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp").setAllowedOrigins("http://?").withSockJS();
     }
 
+    // 메시지 브로커 구성
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 클라이언트가 구독할 대상 설정
         registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
+        // 클라이언트가 발행한 메시지의 목적지(prefix) 설정
+        registry.setApplicationDestinationPrefixes("/api/chatting");
     }
 }
